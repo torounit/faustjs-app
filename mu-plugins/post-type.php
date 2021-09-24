e586fba8d808a6b655b8a078d948bf80da9a314c
Hiroshi Urabe <mail@torounit.com>
2021-09-24 16:50:33 +0900

    add plugins

--- mu-plugins/post-type.php
+++ mu-plugins/post-type.php
@@ -17,6 +17,18 @@ add_action('init', function () {
         'graphql_plural_name' => 'books',
         'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt','revisions')
     ]);
+
+    register_taxonomy( 'genre', 'book', [
+        'labels'  => [
+            'menu_name' => __( 'Genres', 'your-textdomain' ), //@see https://developer.wordpress.org/themes/functionality/internationalization/
+        ],
+        'show_in_graphql' => true,
+        'public' => true,
+        'show_in_rest' => true,
+        'show_in_quick_edit' => true,
+        'graphql_single_name' => 'genre',
+        'graphql_plural_name' => 'genres',
+    ]);
 });
 
 add_filter('preview_post_link', function ($link, WP_Post $post) {
